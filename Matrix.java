8fb54050979d8bf209e97f10c2025e8b92edc41c
githubramjay <ramjay@umich.edu>
2017-11-15 12:16:03 +0000

    v1.1

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,3 @@
-
 public class Matrix {
 	private static double[][] values;
 	
@@ -31,28 +30,44 @@ public class Matrix {
 		}
 	}
 	
-	public Matrix insert(double target) {
-		return null;
+	public void insert(double target, int row, int column) {
+		values[row - 1][column - 1] = target;
+	}
+	
+	public void append(double[] new_row) {
+		
 	}
 	
 	public void remove(int row, int column) {
-		
+		values[row - 1][column - 1] = 0;
 	}
 	
 	public double get(int row, int column) {
-		return 0;
+		return values[row - 1][column - 1];
 	}
 	
 	public void empty() {
-		
+		values = new double[values.length][values[0].length];
 	}
 	
 	public void swap_row(int row_a, int row_b) {
-		
+		row_a -= 1;
+		row_b -= 1;
+		double[] temp = values[row_a];
+		for (int i = 0; i < values.length; i++) {
+			values[row_a][i] = values[row_b][i];
+			values[row_b][i] = temp[i];
+		}
 	}
 	
 	public void swap_column(int column_a, int column_b) {
-		
+		column_a -= 1;
+		column_b -= 1;
+		double[] temp = values[column_a];
+		for (int i = 0; i < values.length; i++) {
+			values[i][column_a] = values[i][column_b];
+			values[i][column_b] = temp[i];
+		}
 	}
 	
 	public int[][][] search(double target) {
